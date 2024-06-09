6b77f12c16ae18574aec7de7ed5cfe9add7da723
Олександр Казаков <Oleksandr.Kazakov@infiz.khpi.edu.ua>
2024-06-09 16:29:20 +0300

    Добавьте файлы проекта.

---
+++ lab13/lab13.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int geometricSeries(int n) {
+    if (n == 1) {
+        return 1;
+    }
+    return 3 * geometricSeries(n - 1);
+}
+
+int main() {
+    int n;
+    cout << "Enter the row member number: ";
+    cin >> n;
+
+    int result = geometricSeries(n);
+    cout << n << "-th member of the series " << result << endl;
+
+    return 0;
+}
